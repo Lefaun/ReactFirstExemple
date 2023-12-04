bd2d407ad0090531ec581563e4d184703f66f126
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-12-04 16:59:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
 ReactDOM.render(<div> <img src="https://pbs.twimg.com/media/DCxB3VNWsAI8VUc.jpg" width="100%"/>
