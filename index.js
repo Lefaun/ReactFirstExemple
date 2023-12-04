eb1a0730438b6808b34d2b92b6fd0e304d028563
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-12-04 17:02:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+function App(){
+  return(
+    <div> 
+    <h1> There´s a Lot of Reasons to Learn React </h1>
+    <img src="https://pbs.twimg.com/media/DCxB3VNWsAI8VUc.jpg" width="100%"/>
+    </div>
+    
+  )
+}
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
-ReactDOM.render(<div> <img src="https://pbs.twimg.com/media/DCxB3VNWsAI8VUc.jpg" width="100%"/>
-</div>, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
