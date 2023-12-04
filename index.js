6692e1c34bfde8479bad06efebe47dcbde0b8cba
Lefaun <118660975+Lefaun@users.noreply.github.com>
2023-12-04 16:52:32 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// Spend a minute memorizing the line below
+// Re-write the line of code as best you can from memory
+ReactDOM.render(<div> <img src="https://pbs.twimg.com/media/DCxB3VNWsAI8VUc.jpg" width="100%"/>
+</div>, document.getElementById("root"))
